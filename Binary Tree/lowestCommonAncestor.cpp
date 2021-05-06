b1c91e820c70c548e5a13a9047702cb397270756
Nischay <nischay26.scs@gmail.com>
2021-05-06 23:17:04 +0530

    Added lowestCommonAncestor.cpp

---
+++ Binary Tree/lowestCommonAncestor.cpp
@@ -0,0 +1,23 @@
+/*
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node( int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+int32_t main() {
+    
+}
