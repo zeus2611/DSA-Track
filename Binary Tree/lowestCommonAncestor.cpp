f58f07e42ff4fa2e2ed2249a5a936fd7c66d9fbd
Nischay <nischay26.scs@gmail.com>
2021-05-07 11:54:15 +0530

    update lowestCommonAncestor.cpp

--- Binary Tree/lowestCommonAncestor.cpp
+++ Binary Tree/lowestCommonAncestor.cpp
@@ -1,5 +1,5 @@
 /*
-
+LCA = Point of path change when we go from root to node
 */
 
 #include<bits/stdc++.h>
@@ -17,7 +17,43 @@ struct Node {
     }
 };
 
+bool getPath(Node* root, int k, vector<int> path){
+    if(root == NULL){
+        return false;
+    }
+    path.push_back(root->data);
+    if(root->data == k){
+        return true;
+    }
+    if(getPath(root->left, k, path) || getPath(root->right, k, path)){
+        return true;
+    }
+
+    path.pop_back();
+    return false;
+}
+
+int LCA(Node* root, int n1, int n2){
+    vector<int> path1, path2;
+
+    if(!getPath(root, n1, path1) || !getPath(root, n2, path2)){
+        return -1;
+    }
+
+    int pc; // pc= pathChange
+    for(pc = 0; pc<path1.size() && path2.size(); pc++){
+        if(path1[pc] != path2[pc]){
+            break;
+        }
+    }
+
+    return path1[pc-1];
+}
+
+Node* optimizedLCA(){
+    
+}
 
 int32_t main() {
-    
+
 }
